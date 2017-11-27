35c4c6d7a663e411e3820ce08e263b1b311c3f82
Breno Henrique <brenosc2@hotmail.com>
2017-11-26 23:34:36 -0200

    adding mutation to add, delete and edit customers

--- schema.js
+++ schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphNonNull } = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull } = require('graphql')
 const axios = require('axios')
 
 const CustomerType = new GraphQLObjectType({
@@ -36,6 +36,52 @@ const RootQuery = new GraphQLObjectType({
 
 })
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) }
+      },
+      resolve (parentValue, args) {
+        return axios.post('http://localhost:3000/customers', {
+          name: args.name,
+          email: args.email,
+          age: args.age
+        })
+          .then(res => res.data)
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve (parentValue, args) {
+        return axios.delete(`http://localhost:3000/customers/${args.id}`)
+          .then(res => res.data)
+      }
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve (parentValue, args) {
+        return axios.patch(`http://localhost:3000/customers/${args.id}`, args)
+          .then(res => res.data)
+      }
+    },
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation,
 })
